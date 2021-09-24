f915a6ede455788cdf116da05d703d6b352c1ce5
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-09-24 18:20:01 -0500

    PracticaPresencial
    .

---
+++ AppsPresencial/AppsPresencial/Controllers/RegistroController.cs
@@ -0,0 +1,39 @@
+﻿using AppsPresencial.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppsPresencial.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Indexxx()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+
+        }
+
+        public IActionResult Index(ClsValidez Validacion)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("Registro");
+            }
+               
+
+
+            else
+            {
+                return View("Index");
+            }
+
+        }
+    }
+}
---
+++ AppsPresencial/AppsPresencial/Models/ClsValidez.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppsPresencial.Models
+{
+    public class ClsValidez
+    {
+         [Display(Name ="Nombre")]
+         [Required(ErrorMessage ="Este campo es requerido")]
+         public string Nombre { get; set; }
+
+
+
+
+
+        [Display(Name ="Edad")]
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public int Edad { get; set; }
+
+
+
+        [Display(Name ="Sexo")]
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public String Sexo { get; set; }
+
+
+
+
+
+        [Display(Name ="TipoDeEstudio")]
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public String TipoDeEstudio { get; set; }
+    }
+}
